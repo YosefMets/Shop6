f76b5ece14eab55d45208f01fa1f2d24495701e3
Yosef Mets <maxmets@gmail.com>
2025-03-05 14:05:32 +0100

    Modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
-import { i18n } from './configs/i18n'
-import { image } from './configs/image'
-import { stripe } from './configs/stripe'
+// import { i18n } from './configs/i18n'
+// import { image } from './configs/image'
+// import { stripe } from './configs/stripe'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -9,9 +9,6 @@ export default defineNuxtConfig({
     '@nuxthub/core',
     '@nuxt/eslint',
     ['@pinia/nuxt', { autoImports: ['defineStore', 'storeToRefs'] }],
-    ['@nuxtjs/i18n', i18n],
-    ['@nuxt/image', image],
-    ['@unlok-co/nuxt-stripe', stripe],
   ],
 
   // https://devtools.nuxt.com
